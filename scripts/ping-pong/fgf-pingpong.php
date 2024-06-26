0a9b0523b757f0b942d14322fb330a5ea58337d9
Fabien Udriot <fabien@omic.ch>
2024-06-26 18:23:10 +0200

    chore: ping-pong script

--- scripts/ping-pong/fgf-pingpong.php
+++ scripts/ping-pong/fgf-pingpong.php
@@ -1,61 +1,58 @@
 #!/usr/bin/env php
 <?php
-
 /*
 Script CLI (command line interface) for synchronizing two database
 */
 
 $synchronizeTables = [
     'pages',
-//    'tt_content',
-//    'be_groups',
-//    'be_users',
-//    'fe_groups',
-//    'fe_users',
-//    'sys_refindex',
-//    'sys_template',
-//    'tx_datafilter_filters',
-//    'tx_dataquery_queries',
-//    'tx_displaycontroller_components_mm',
-//    'tx_phpdisplay_displays',
+    //    'tt_content',
+    //    'be_groups',
+    //    'be_users',
+    //    'fe_groups',
+    //    'fe_users',
+    //    'sys_refindex',
+    //    'sys_template',
+    //    'tx_datafilter_filters',
+    //    'tx_dataquery_queries',
+    //    'tx_displaycontroller_components_mm',
+    //    'tx_phpdisplay_displays',
 ];
 
 $synchronizeTablesDefaultValues = [
     'pages' => [
-
         'media' => 0,
-//        'subtitle',
-//        'author',
-//        'nav_title',
+        //        'subtitle',
+        //        'author',
+        //        'nav_title',
     ],
-//    'tt_content' => [
-//
-//        'subheader',
-//        'header_link'
-//    ],
-//    'be_groups' => [],
-//    'be_users' => [
-//        'password'
-//    ],
-//    'fe_groups' => [],
-//    'fe_users' => [],
-//    'sys_domain' => [],
-//    'sys_refindex' => [],
-//    'sys_template' => [],
-//    'tx_datafilter_filters' => [],
-//    'tx_dataquery_queries' => [],
-//    'tx_displaycontroller_components_mm' => [],
-//    'tx_phpdisplay_displays' => [],
+    //    'tt_content' => [
+    //
+    //        'subheader',
+    //        'header_link'
+    //    ],
+    //    'be_groups' => [],
+    //    'be_users' => [
+    //        'password'
+    //    ],
+    //    'fe_groups' => [],
+    //    'fe_users' => [],
+    //    'sys_domain' => [],
+    //    'sys_refindex' => [],
+    //    'sys_template' => [],
+    //    'tx_datafilter_filters' => [],
+    //    'tx_dataquery_queries' => [],
+    //    'tx_displaycontroller_components_mm' => [],
+    //    'tx_phpdisplay_displays' => [],
 ];
 
-
-$importTables = array(
+$importTables = [
     //'pages',
-//	'tt_content',
-//	'sys_file',
-//	'sys_file_reference',
-//	'be_users',
-);
+    //	'tt_content',
+    //	'sys_file',
+    //	'sys_file_reference',
+    //	'be_users',
+];
 ############################################
 # Database credentials
 ############################################
@@ -76,39 +73,48 @@ $targetCredentials = [
     'port' => '3306',
 ];
 
-
 ############################################
 # Beginning of the script
 ############################################
 
-include('Classes/Database.php');
-include('Classes/Logger.php');
-include('Credentials.php');
+include 'Classes/Database.php';
+include 'Classes/Logger.php';
+include 'Credentials.php';
 
 $logger = new Logger();
 
-$dbSource = new Ecodev\Database($sourceCredentials['host'], $sourceCredentials['username'], $sourceCredentials['password'], $sourceCredentials['port']);
+$dbSource = new Ecodev\Database(
+    $sourceCredentials['host'],
+    $sourceCredentials['username'],
+    $sourceCredentials['password'],
+    $sourceCredentials['port'],
+);
 $dbSource->connect($sourceCredentials['database']);
 
-$dbTarget = new Ecodev\Database($targetCredentials['host'], $targetCredentials['username'], $targetCredentials['password'], $targetCredentials['port']);
+$dbTarget = new Ecodev\Database(
+    $targetCredentials['host'],
+    $targetCredentials['username'],
+    $targetCredentials['password'],
+    $targetCredentials['port'],
+);
 $dbTarget->connect($targetCredentials['database']);
 
 // Synchronize tables
 foreach ($synchronizeTables as $synchronizeTable) {
     $logger->log('Synchronizing table "' . $synchronizeTable . '" .....');
     $fieldStructures = $dbTarget->select('SHOW COLUMNS FROM ' . $synchronizeTable);
-    $newFieldsNames = array();
+    $newFieldsNames = [];
     foreach ($fieldStructures as $fieldStructure) {
         $newFieldsNames[] = $fieldStructure['Field'];
     }
 
     // Build clause part of the request
     $clause = '1 = 1';
-    $specialFields = array(
+    $specialFields = [
         'deleted',
-//        'disable',
-//        'hidden'
-    );
+        //        'disable',
+        //        'hidden'
+    ];
     foreach ($specialFields as $specialField) {
         if (in_array($specialField, $newFieldsNames)) {
             $clause .= ' AND ' . $specialField . ' = 0';
@@ -123,7 +129,6 @@ foreach ($synchronizeTables as $synchronizeTable) {
     $dbNew->query('TRUNCATE TABLE '. $table); => other way to do it
     */
     foreach ($toImportValues as $index => $toImportValue) {
-
         // We sanitize the data by removing the unwanted fields
         foreach ($toImportValue as $fieldName => $value) {
             if (!in_array($fieldName, $newFieldsNames)) {
@@ -145,23 +150,34 @@ $logger->log('All tables synchronized successfully!!!');
 // Import tables
 foreach ($importTables as $importTable) {
     $logger->log('Import whole content of table: "' . $importTable . '"');
-    $exportCommand = sprintf('mysqldump -u %s -p"%s" %s %s > /tmp/%s.sql',
+    $exportCommand = sprintf(
+        'mysqldump -u %s -p"%s" %s %s > /tmp/%s.sql',
         $sourceCredentials['username'],
         $sourceCredentials['password'],
         $sourceCredentials['database'],
         $importTable,
-        $importTable
+        $importTable,
     );
 
     exec($exportCommand);
-    $importCommand = sprintf('mysql -u %s -p"%s" %s < /tmp/%s.sql',
+    $importCommand = sprintf(
+        'mysql -u %s -p"%s" %s < /tmp/%s.sql',
         $targetCredentials['username'],
         $targetCredentials['password'],
         $targetCredentials['database'],
-        $importTable
+        $importTable,
     );
 
     exec($importCommand);
     $logger->log('Done for: ' . $importTable . '!!!');
 }
+
 $logger->log('All tables imported successfully in the new database!!!');
+
+$dbTarget->update(
+    'pages',
+    ['slug' => '/', 'backend_layout' => 'pagets__default', 'backend_layout_next_level' => 'pagets__default', 'TSconfig' => ''],
+    ['uid' => '1'],
+);
+$dbTarget->update('pages', ['slug' => '/home'], ['uid' => '7']);
+$dbTarget->update('pages', ['slug' => '/zugang-life-archivech'], ['uid' => '21']);
